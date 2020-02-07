784b60853f6403d6d431839edbb8cccc2871afa0
Vince Suriani <chenzorama@hotmail.com>
2020-02-07 15:45:59 -0500

    first commit :tada:

---
+++ README.md
@@ -0,0 +1,1 @@
+# Mr. Chenzo's blog
---
+++ config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // the name of your website
+  title: 'Mr. Chenzo',
+  // the description of the website (eg. what shows on Google)
+  description: "Mr. Chenzo's blog",
+  // a short bio shown at the bottom of your blog posts
+  // It should complete the sentence: Written by Mr. Chenzo ...
+  shortBio: '',
+  // a longer bio showing on the landing page of the blog
+  bio: `Part Time Pirate.`,
+  author: 'Mr. Chenzo',
+  githubUrl: 'https://github.com/Chenzo/chenzo_blog',
+  // replace this by the url where your website will be published
+  siteUrl: 'http://localhost:8000',
+  social: {
+    // leave the social media you do not want to appear as empty strings
+    twitter: '1Chenzo',
+    medium: '@Chenzo',
+    facebook: '',
+    github: '',
+    linkedin: '',
+    instagram: '',
+  },
+}
